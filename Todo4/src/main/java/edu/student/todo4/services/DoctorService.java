a209a105408de0e28f5798cbc7b338ef644972e7
Momen-Assaf <momenssf@gmail.com>
2024-05-30 15:49:14 +0300

    paitent service

--- Todo4/src/main/java/edu/student/todo4/services/DoctorService.java
+++ Todo4/src/main/java/edu/student/todo4/services/DoctorService.java
@@ -1,4 +1,13 @@
 package edu.student.todo4.services;
 
+import edu.student.todo4.models.Doctor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DoctorService {
+    private ArrayList<Doctor> doctors = new ArrayList<Doctor>(Arrays.asList(
+            new Doctor(1L,"Rani",30,5,33.2),
+            new Doctor(2L, "Mohammad", 10, 10, 3)
+    ));
 }
--- Todo4/src/main/java/edu/student/todo4/services/PatientService.java
+++ Todo4/src/main/java/edu/student/todo4/services/PatientService.java
@@ -1,4 +1,30 @@
 package edu.student.todo4.services;
 
+import edu.student.todo4.models.Patient;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class PatientService {
+    private ArrayList<Patient> patients = new ArrayList<Patient>(Arrays.asList(
+            new Patient(1L,"Momen Assaf",32.2,false, new ArrayList<Long>(Arrays.asList(1L,2L))),
+            new Patient(2L, "Salameh", 2, true,new ArrayList<Long>(Arrays.asList(1L)))
+    ));
+
+    public ArrayList<Patient> getPatients() {
+        return this.patients;
+    }
+
+    public boolean addPatient(Patient patient) {
+        return this.patients.add(patient);
+    }
+
+    public String getPatient(long id) {
+        for(int i = 0; i < patients.size(); i++){
+            if(patients.get(i).getId() == id)return patients.get(i).toString();
+        }
+        return "There is no such patient";
+    }
+
 }
