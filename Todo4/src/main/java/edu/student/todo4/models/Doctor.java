5302e0aa0a1645db106c30ab00ff2644ddcbd5f2
Momen-Assaf <momenssf@gmail.com>
2024-05-30 15:19:58 +0300

    Todo 4 initilized

---
+++ Todo4/src/main/java/edu/student/todo4/models/Doctor.java
@@ -0,0 +1,4 @@
+package edu.student.todo4.models;
+
+public class Doctor {
+}
---
+++ Todo4/src/test/java/edu/student/todo4/Todo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.student.todo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
